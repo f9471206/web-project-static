42700bb8c8bffab2e4f85838a92c6bf65a4d46f7
f9471206 <f9471206@gmail.com>
2023-09-11 14:02:47 +0800

    userimg

--- src/components/replys-component.js
+++ src/components/replys-component.js
@@ -140,6 +140,8 @@ const ReplysComponent = ({ newReply }) => {
             <div key={data._id} className="reply_main">
               <div className="reply_left">
                 <div className="reply_photo">
+                  {!data.user.photo ||
+                    (!data.user && <img src={defaulephoto} alt="" />)}
                   {data.user.photo ? (
                     <img src={data.user.photo} alt="" />
                   ) : (
