a1c49e1ffd39d123a4a2a180bb508004a3597dc9
f9471206 <f9471206@gmail.com>
2024-03-04 01:11:45 +0800

    debug

--- src/components/home-components.js
+++ src/components/home-components.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,6 +6,7 @@ import Button from "react-bootstrap/esm/Button";
 import HomeArticleComponents from "./home-article-components";
 import SortComponents from "./modals/sort-components";
 import TagComponents from "./modals/tag-components";
+import { useLocation, useParams } from "react-router-dom";
 import queryString from "query-string";
 import HomeService from "../services/home.service";
 import ModalsPlaceholderComponents from "./modals/modals-placeholder-components";
@@ -15,25 +15,28 @@ import Badge from "react-bootstrap/Badge";
 
 const homeComponents = ({ data, setData }) => {
   const { _tag } = useParams();
-  let location = useLocation();
-  const [refresh, setRefresh] = useState(false);
-  const [placeholder, setPlaceholder] = useState(false);
-  let sort = queryString.parse(location.search);
 
-  useEffect(() => {
-    if (data) return;
-    setPlaceholder(true);
-    HomeService.home(_tag, sort)
-      .then((d) => {
-        setData(d.data);
-        setPlaceholder(false);
-      })
-      .catch((e) => {
-        console.log(e);
-        setPlaceholder(false);
-      });
-  }, []);
-  // _tag, location, refresh, data
+  const [refresh, setRefresh] = useState(false);
+
+  let location = useLocation().search;
+  let sort = queryString.parse(location);
+
+  const [placeholder, setPlaceholder] = useState(false);
+
+  // useEffect(() => {
+  //   if (data) return;
+  //   setPlaceholder(true);
+  //   HomeService.home(_tag, sort)
+  //     .then((d) => {
+  //       setData(d.data);
+  //       setPlaceholder(false);
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //       setPlaceholder(false);
+  //     });
+  // }, [_tag, location, refresh, data]);
+
   return (
     <>
       {_tag && (
